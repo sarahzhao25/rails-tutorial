858c8d69ec2ad549761f88b767322302cc4fd9c7
Sarah Zhao <sarahzhao25@gmail.com>
2018-07-17 14:27:27 -0400

    created seed file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+  Blog.create!(
+    title: "Sarah's blog post #{blog}",
+    body: "More Posts"
+  )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+  )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "This is the title #{portfolio_item}",
+    subtitle: "This is the subtitle",
+    body: "Etc",
+    main_image: "http://placehold.it/350x400",
+    thumb_image: "http://placehold.it/300x150"
+  )
+end
+
+puts "9 portfolios created"
