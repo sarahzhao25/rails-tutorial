8d0ef005d0c1f810c566234aa15ead222fe82829
Sarah Zhao <sarahzhao25@gmail.com>
2018-07-18 16:35:24 -0400

    implemented concern for managing image adata

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,17 +1,18 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body
 
   def self.angular #this adds a property onto the portfolio class
     where(subtitle: 'Angular')
   end
 
-  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') } #can be called anything, the -> is a lambda?, can also create a property?
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Raimails') } #can be called anything, the -> is a lambda?, can also create a property?
 
   #custom scopes to call to the controller
   after_initialize :set_defaults
 
-  def :set_defaults
-    self.main_image ||= "http://placehold.it/600x400" # this is saying "self.main_image is itself or the second one"
-    self.thumb_image ||= "http://placehold.it/350x200"
+  def set_defaults
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400') # this is saying "self.main_image is itself or the second one"
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 end
