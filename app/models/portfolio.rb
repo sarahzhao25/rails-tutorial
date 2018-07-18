a0677e8ea529f042c501ece39ee16a44def0d5c1
Sarah Zhao <sarahzhao25@gmail.com>
2018-07-18 15:56:33 -0400

    implemented custom scopes for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,11 @@
 class Portfolio < ApplicationRecord
-  validates_presence_of : :title, :body
+  validates_presence_of :title, :body
+
+  def self.angular #this adds a property onto the portfolio class
+    where(subtitle: 'Angular')
+  end
+
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') } #can be called anything, the -> is a lambda?, can also create a property?
+
+  #custom scopes to call to the controller
 end
