fd3dcfcbde135de612d1b52c2f351739270aa580
Sarah Zhao <sarahzhao25@gmail.com>
2018-07-18 16:18:45 -0400

    updated data defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,4 +8,10 @@ class Portfolio < ApplicationRecord
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') } #can be called anything, the -> is a lambda?, can also create a property?
 
   #custom scopes to call to the controller
+  after_initialize :set_defaults
+
+  def :set_defaults
+    self.main_image ||= "http://placehold.it/600x400" # this is saying "self.main_image is itself or the second one"
+    self.thumb_image ||= "http://placehold.it/350x200"
+  end
 end
