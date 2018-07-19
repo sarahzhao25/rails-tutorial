a8f7fa1361960b0734c4d70e8a438360dde9bac3
Sarah Zhao <sarahzhao25@gmail.com>
2018-07-19 14:45:45 -0400

    completed authentication with virtual attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :name
+
+  def first_name
+    #split naturally splits via a space in the string
+    #is this an implicit return?
+    self.name.split.first
+  end
+
+  def last_name
+    self.name.split.last
+  end
+
 end
