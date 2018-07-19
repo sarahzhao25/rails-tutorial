8266aaab785f1f13fb738b62ad723a1e9f020678
Sarah Zhao <sarahzhao25@gmail.com>
2018-07-19 10:29:20 -0400

    added nested attributes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,6 +9,8 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
+    #build = instantiate 3 versions of the portfolio item's technologies and make them available to the form
+    3.times { @portfolio_item.technologies.build }
   end
 
   def show
@@ -16,7 +18,7 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])) #if you see anything coming in under tech attibutes flag with a name, then let it continue. "strong params" are a security
 
     respond_to do |format|
       if @portfolio_item.save
